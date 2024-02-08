3e0351691fb8f57c86508cbba3de12b62b74ec40
evanlin23 <evanlin366@gmail.com>
2024-02-08 17:32:24 -0600

    Add Next ui package for better stylings

--- admin-api-frontend/src/App.js
+++ admin-api-frontend/src/App.js
@@ -5,21 +5,25 @@ import RemoveRolesForm from "./components/FormsFolder/RemoveRolesForm/index.tsx"
 import CreateUserForm from "./components/FormsFolder/CreateUserForm/index.tsx";
 import DeleteUserForm from "./components/FormsFolder/DeleteUserForm/index.tsx";
 
+import { NextUIProvider } from "@nextui-org/react";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>ACM Admin API </p>
-        <p>also roles should be comma separated </p>
-        <div className="Form-container">
-          <CreateUserForm />
-          <DeleteUserForm />
-          <GetUserInfoForm />
-          <RemoveRolesForm />
-          <AddRolesForm />
-        </div>
-      </header>
-    </div>
+    <NextUIProvider>
+      <div className="App">
+        <header className="App-header">
+          <p>ACM Admin API </p>
+          <p>also roles should be comma separated </p>
+          <div className="Form-container">
+            <CreateUserForm />
+            <DeleteUserForm />
+            <GetUserInfoForm />
+            <RemoveRolesForm />
+            <AddRolesForm />
+          </div>
+        </header>
+      </div>
+    </NextUIProvider>
   );
 }
 
