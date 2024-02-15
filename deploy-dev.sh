7f21ebb01a51724a144a1205789f05b518c54736
PranjalChaitanya <44535826+PranjalChaitanya@users.noreply.github.com>
2024-02-15 17:13:59 -0600

    Update deploy-dev.sh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sam build --template-file cloudformation/lambda.yml
-sam deploy --no-confirm-changeset --no-fail-on-empty-changeset --parameter-overrides ParameterKey=UseCustomDomainName,ParameterValue=false ParameterKey=Env,ParameterValue=dev
+sam deploy --no-confirm-changeset --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides ParameterKey=UseCustomDomainName,ParameterValue=false ParameterKey=Env,ParameterValue=dev
