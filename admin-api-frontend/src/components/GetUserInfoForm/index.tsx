404f4c9d879482f078f64804496b5dc1f3da1a85
evanlin23 <evanlin366@gmail.com>
2024-03-28 17:09:04 -0500

    Change get user request to get instead of put

--- admin-api-frontend/src/components/GetUserInfoForm/index.tsx
+++ admin-api-frontend/src/components/GetUserInfoForm/index.tsx
@@ -6,7 +6,7 @@ const GetUserInfoForm = () => {
 
   const handleGetUser = async () => {
     try {
-      const response = await axios.put(
+      const response = await axios.get(
         `${BASE_URL}/default/api/v1/get_user`,
         null,
         {
