1b5bdcac1b6a1b2d140362e01ae4ffdabad02336
kaushik327 <kaushik.r.varadharajan@gmail.com>
2024-04-01 01:16:34 -0500

    lambda authorizer and cf

---
+++ backend/auth/authorizer.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from auth_utils import AuthPolicy
+
+def lambda_handler(event, context):
+    """Do not print the auth token unless absolutely necessary """
+    method, token = event['authorizationToken'].split(' ')
+    #print("Client token: " + event['authorizationToken'])
+    print("Method ARN: " + event['methodArn'])
+    
+    principalId = token
+
+    tmp = event['methodArn'].split(':')
+    apiGatewayArnTmp = tmp[5].split('/')
+    awsAccountId = tmp[4]
+
+    policy = AuthPolicy(principalId, awsAccountId)
+    policy.restApiId = apiGatewayArnTmp[0]
+    policy.region = tmp[3]
+    policy.stage = apiGatewayArnTmp[1]
+
+    # TODO: call Azure to determine if user is allowed to use API
+    policy.allowAllMethods() # For now just let anyone in lol
+
+    # Finally, build the policy
+    authResponse = policy.build()
+ 
+    # context = {
+    #     'key': 'value', # $context.authorizer.key -> value
+    #     'number' : 1,
+    #     'bool' : True
+    # }
+    # authResponse['context'] = context
+    
+    return authResponse
