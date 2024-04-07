ba81b710f30ebf8450caa0498426ad4aef932425
kaushik327 <kaushik.r.varadharajan@gmail.com>
2024-04-06 19:51:44 -0500

    authorizer verifies user; graph api doesnt work yet

--- backend/auth/authorizer.py
+++ backend/auth/authorizer.py
@@ -2,6 +2,65 @@ from __future__ import print_function
 from auth_utils import AuthPolicy
 import json
 import requests
+from jose import jwt
+
+# Constants
+CLIENT_ID = '86baa161-ea6e-4c68-a75a-9ff13690f7da'
+TENANT_ID = 'c8d9148f-9a59-4db3-827d-42ea0c2b6e2e'
+ADMIN_API_USER_GROUP_OBJECT_ID = '05de763f-9468-4f98-944b-21c20aee9818'
+CLIENT_SECRET = '' # TODO: use secrets manager
+
+def get_user_object_id(token): 
+    # https://github.com/Azure-Samples/ms-identity-python-webapi-azurefunctions/blob/master/Function/secureFlaskApp/__init__.py   
+    keys_req = requests.get(f"https://login.microsoftonline.com/{TENANT_ID}/discovery/v2.0/keys")
+    keys = keys_req.json()['keys']
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    for key in keys:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }    
+    if not rsa_key:
+        raise Exception('No RSA key!')
+    payload = jwt.decode(
+        token,
+        rsa_key,
+        algorithms=['RS256'],
+        audience=CLIENT_ID,
+        issuer=f"https://login.microsoftonline.com/{TENANT_ID}/v2.0",
+    )
+    return payload['oid']
+
+
+def get_graph_api_token():
+    # Acquiring Graph API token
+    graph_token_req = requests.post(
+        f'https://login.microsoftonline.com/{TENANT_ID}/oauth2/token',
+        data={
+            'grant_type': 'client_credentials',
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'scope': 'https://graph.microsoft.com/.default'
+        }
+    )
+    print(json.dumps(graph_token_req.json(), indent=4))
+    return graph_token_req.json()['access_token']
+
+def get_group_list(user_object_id, graph_api_token):
+    # Currently gives "Invalid audience" error
+    member_req = requests.get(
+        f"https://graph.microsoft.com/v1.0/users/{user_object_id}/memberOf",
+        headers={
+            'Authorization': f'Bearer {graph_api_token}',
+            'Content-Type': 'application/json'
+        })
+    return member_req.json()
+
 
 def lambda_handler(event, context):
     """Do not print the auth token unless absolutely necessary """
@@ -20,35 +79,35 @@ def lambda_handler(event, context):
     policy.region = tmp[3]
     policy.stage = apiGatewayArnTmp[1]
 
-    # TODO: call Azure to determine if user is allowed to use API
-    # if valid user, then allowAllMethods
-    #tenant_id??
-
-    # Define the user's object ID and the group's object ID
-    user_object_id = '8412d538-534d-45f0-9a69-6b52eaf39560'
-    group_object_id = 'GROUP_OBJECT_ID'
-
-    # Create a Confidential Client Application instance
-
-    # Check if the user is a member of the group
-    url = f"https://graph.microsoft.com/v1.0/groups/{group_object_id}/members/{user_object_id}/$ref"
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 204:
-        print("User is a member of the group.")
-        policy.allowAllMethods()
-    elif response.status_code == 404:
+    try:
+        user_object_id = get_user_object_id(token)
+    except Exception:
         raise Exception('Unauthorized')
-    else:
-        # TODO: this returns a 500 error I think? is there a better way to do this
-        return "Error occurred while checking group membership."
+
+    print(f'User object id: {user_object_id}')
+
+    # try:
+    #     graph_api_token = get_graph_api_token()
+    #     group_list = get_group_list(user_object_id, graph_api_token)
+    # except Exception:
+    #     raise Exception('Graph API error')
+    
+    # print(f'Group object list: {group_list}')
+    # if ADMIN_API_USER_GROUP_OBJECT_ID not in group_list:
+    #     raise Exception('Unauthorized')
+    
+    policy.allowAllMethods()
 
     # Finally, build the policy
     authResponse = policy.build()
     print("Auth response: " + json.dumps(authResponse, indent=4))
     
     return authResponse
+
+if __name__ == '__main__':
+    # the tokens are literally too long for my shell, so i needed to split the Bearer token to input it all for testing
+    token = input() + input()
+    lambda_handler({
+        'authorizationToken': f'Bearer {token}',
+        'methodArn': 'arn:aws:execute-api:us-east-1:427040638965:x7aneaby6i/Stage/GET/api/v1/get_user'
+    }, None)
